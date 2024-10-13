2b75b426e44519826b4fc87d0e3edf8a2386ba47
wy-xa <wy@xa.com>
2024-10-13 20:22:36 +0800

    1

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,5 +1,5 @@
 <template>
-    <el-card style="min-width: 320px; min-height: 130px;" shadow="always">
+    <el-card style="min-width: 340px; min-height: 130px;" shadow="always">
       <template #header>
         <div class="card-header">
        <el-text class="mx-1" size="large">{{ title }}</el-text>
