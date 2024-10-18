d43e4b2e79b4d210fb08508085980dcac78fc918
cloudyan2012 <55647147+cloudyan2012@users.noreply.github.com>
2024-10-18 19:41:02 +0800

    Update OnlineDev.vue

--- src/components/OnlineDev.vue
+++ src/components/OnlineDev.vue
@@ -5,7 +5,14 @@
       <el-button>{{d5g.dname}}</el-button>
   </el-badge>
   <el-badge v-for="d2g in dev2g" :key="d2g.ip" value="2G" class="item" color="rgb(148.6, 212.3, 117.1)">
+     <el-tooltip
+        class="box-item"
+        effect="dark"
+        :content="d2g.ip"
+        placement="top-start"
+      >   
       <el-button>{{d2g.dname}}</el-button>
+         </el-tooltip>
   </el-badge>
 </el-space>
 </template>
