6eb2a678b27ac311bd95d7d0340b4602c1c030f1
cloudyan2012 <55647147+cloudyan2012@users.noreply.github.com>
2025-06-30 11:13:05 +0000

    Deploying to gh-pages from @ cloudyan2012/opweb_vue@d92cdff80ec8cbdde3c832a6d5e743582ab40a56 🚀

--- assets/Logout-CfEQHzW8.js
+++
@@ -1,1 +0,0 @@
-import{bk as e,bn as o}from"./index-DCHiyfn3.js";const r={setup(){const t=o();localStorage.removeItem("token"),t.push("/")}};function s(t,n,c,u,a,p){return null}const f=e(r,[["render",s]]);export{f as default};
